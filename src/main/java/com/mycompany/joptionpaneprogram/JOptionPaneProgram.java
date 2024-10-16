6c7a29365f16e792cf2bd4dee7ba2eef1274a297
RC_Student_lab <RC_Student_lab@10.0.0.15>
2024-10-16 17:49:15 +0000

    first commit

---
+++ src/main/java/com/mycompany/joptionpaneprogram/JOptionPaneProgram.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.joptionpaneprogram;
+
+import java.util.Scanner;
+import javax.swing.*;
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class JOptionPaneProgram {
+
+    public static void main(String[] args) {
+       Scanner input=new Scanner(System.in);
+       
+       //Declarations
+       int b = 4,c = 7;
+       
+       String num1, num2;
+       int product=b*c;
+       int division=b/c;
+      
+       
+        
+       
+     
+       
+       
+     }
+    }
+
+
